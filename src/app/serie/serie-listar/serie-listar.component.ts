e72aa6d01de902a22116ecf746dc024320612db7
dcamelog <69636867+dcamelog@users.noreply.github.com>
2021-11-25 15:56:03 -0500

    Update serie-listar.component.ts

--- src/app/serie/serie-listar/serie-listar.component.ts
+++ src/app/serie/serie-listar/serie-listar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Serie } from '../serie';
-import { SerieService } from '../serie.service';
+import { SerieService } from '../serie.service' ;
 
 @Component({
   selector: 'app-serie-listar',
